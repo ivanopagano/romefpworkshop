24c236fadb096ab3fa014e7a24cf15a2e0de0827
Ivano Pagano <pagoda_5b@hotmail.com>
2018-05-23 19:29:41 +0200

    Rewrite Fibonacci using vavr

--- src/test/java/com/agiledeveloper/Fibonacci.java
+++ src/test/java/com/agiledeveloper/Fibonacci.java
@@ -1,39 +1,25 @@
 package com.agiledeveloper;
 
-import java.util.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.*;
+import java.util.List;
 
-class NextInSeries {
-  public final int current;
-  public final int next;
-  
-  public NextInSeries(int theCurrent, int theNext) {
-    current = theCurrent;
-    next = theNext;
-  }
-                   
-  public NextInSeries nextValue() {
-    return new NextInSeries(next, current + next);
-  }
-}
+import io.vavr.Tuple;
+import io.vavr.collection.Stream;
 
 class Fibonacci {
   public static int numberAtPosition(int position) {
     
-    NextInSeries initial = new NextInSeries(0, 1);
+    return Stream.iterate(Tuple.of(0, 1), t -> Tuple.of(t._2, t._1 + t._2))
+      .drop(position)
+      .get()
+      ._2;
     
-    return IntStream.range(0, position)
-     .mapToObj(ignore -> new NextInSeries(0, 0))
-     .reduce(initial, (result, ignore) -> result.nextValue())
-     .next;
   }             
   
   public static List<Integer> seriesUntilPosition(int position) {
-    return IntStream.rangeClosed(0, position)
-      .mapToObj(index -> numberAtPosition(index))
-      .collect(toList());
-  } 
+    return Stream.range(0, position + 1)
+      .map(Fibonacci::numberAtPosition)
+      .toJavaList();
+  }
 }
 
 
